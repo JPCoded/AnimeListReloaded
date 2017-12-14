cf83ef82bb6d17af86a45a3a567665c18d781b81
John Pomeroy <jpcoded@gmail.com>
2017-12-14 15:11:00 -0500

    added genre's dictonary to keep track of how many of each genre i watched.

--- AnimeListReloaded/MainWindow.xaml.cs
+++ AnimeListReloaded/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 using System.Xml.Linq;
 
 namespace AnimeListReloaded
@@ -18,6 +20,19 @@ namespace AnimeListReloaded
         private void BtnLoad_Click(object sender, RoutedEventArgs e)
         {
             var animeElements = GetElements($"http://myanimelist.net/malappinfo.php?u={txtUsername.Text}&status=all&type=anime");
+          
+            var genreDic = new Dictionary<string, int>();
+            foreach (var t in animeElements.Select(anime => anime.Element("my_tags")?.Value).Select(genres => genres.Split(',')).SelectMany(temp => temp))
+            {
+                if (genreDic.ContainsKey(t.Trim()))
+                {
+                    genreDic[t.Trim()] += 1;
+                }
+                else
+                {
+                    genreDic.Add(t.Trim(),1);
+                }
+            }
           
         }
 
